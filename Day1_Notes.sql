99697a5208e3b55cc86d7f78c02bae6eb5aff27d
Richard <richard.yang.tong@gmail.com>
2021-05-27 11:35:02 -0400

    Added notes.

--- Day1_Notes.sql
+++ Day1_Notes.sql
@@ -29,3 +29,16 @@ SELECT 100%98;
 
 #6. 查询函数
 SELECT VERSION();
+
+#7. 起别名
+/*
+1. 便于理解
+2. 如果要查询的字段有重名的情况, 起别名可以区分开来
+*/
+
+#方式一: 使用AS
+SELECT 100%98 AS result;
+SELECT last_name AS lastName, first_anme AS firstName FROM employees;
+
+#方式二: 使用空格
+SELECT last_name lastName, first_name firstName FROM employees;
