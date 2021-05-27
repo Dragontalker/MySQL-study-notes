6d5e0a69334ae320e4125dc64caf9d4846ed9d37
Richard <richard.yang.tong@gmail.com>
2021-05-27 16:28:11 -0400

    Added notes.

--- Notes_03.sql
+++ Notes_03.sql
@@ -2,3 +2,8 @@
 SELECT salary, last_name
 FROM employees
 WHERE commission_pct IS NULL AND salary < 18000;
+
+#2. 查询employees表中, job_id不为'IT'或者 工资为12000的员工信息
+SELECT *
+FROM employees
+WHERE job_id <> 'IT' OR salary = 12000;
