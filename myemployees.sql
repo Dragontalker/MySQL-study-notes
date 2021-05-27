7122c9ed5ea3e8b79468c899c6f44417cbf755a3
Richard <richard.yang.tong@gmail.com>
2021-05-27 11:12:14 -0400

    Added notes.

--- myemployees.sql
+++ myemployees.sql
@@ -3,8 +3,13 @@
 
 USE myemployees;
 
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS jobs;
+DROP TABLE IF EXISTS departments;
+
 CREATE TABLE employees (
-	employee_id INT,
+	employee_id INT AUTO_INCREMENT,
     first_name VARCHAR(20) NULL,
     last_name VARCHAR(25) NULL,
     email VARCHAR(25) NULL,
@@ -19,7 +24,7 @@ CREATE TABLE employees (
 );    
 
 CREATE TABLE departments (
-	department_id INT,
+	department_id INT AUTO_INCREMENT,
 	department_name VARCHAR(3) NULL,
     manager_id INT NULL,
     location_id INT NULL,
@@ -27,7 +32,7 @@ CREATE TABLE departments (
 ); 
 
 CREATE TABLE locations (
-	location_id INT,
+	location_id INT AUTO_INCREMENT,
     street_address VARCHAR(40) NULL,
     postal_code VARCHAR(12) NULL,
     city VARCHAR(30) NULL,
@@ -37,7 +42,7 @@ CREATE TABLE locations (
 );
 
 CREATE TABLE jobs (
-	job_id VARCHAR(10),
+	job_id INT AUTO_INCREMENT,
     job_title VARCHAR(35) NULL,
     min_salary INT NULL,
     max_salary INT NULL,
@@ -54,3 +59,12 @@ CREATE TABLE jobs (
 1. 查询列表可以是： 表中的字段、常量值、表达式、函数
 2. 查询的结果是一个虚拟的表格
 */
+
+#1. 查询表中的单个字段
+SELECT last_name FROM employees;
+
+#2. 查询表中的多个字段
+SELECT last_name, salary, email FROM employees;
+
+#3. 查询表中的所有字段
+SELECT * FROM employees;
