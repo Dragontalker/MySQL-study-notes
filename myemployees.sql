759f89ceee090e9930be2a90b5419c076703ef3a
Richard <richard.yang.tong@gmail.com>
2021-05-27 10:39:28 -0400

    Added jobs table.

--- myemployees.sql
+++ myemployees.sql
@@ -18,5 +18,32 @@ CREATE TABLE employees (
     PRIMARY KEY (employee_id)
 );    
 
+CREATE TABLE departments (
+	department_id INT,
+	department_name VARCHAR(3) NULL,
+    manager_id INT NULL,
+    location_id INT NULL,
+    PRIMARY KEY (department_id)
+); 
+
+CREATE TABLE locations (
+	location_id INT,
+    street_address VARCHAR(40) NULL,
+    postal_code VARCHAR(12) NULL,
+    city VARCHAR(30) NULL,
+    state_province VARCHAR(25) NULL,
+    country_id INT NULL,
+    PRIMARY KEY (location_id)
+);
+
+CREATE TABLE jobs (
+	job_id VARCHAR(10),
+    job_title VARCHAR(35) NULL,
+    min_salary INT NULL,
+    max_salary INT NULL,
+    PRIMARY KEY (job_id)
+);
+
+
 SELECT *
 FROM employees;
