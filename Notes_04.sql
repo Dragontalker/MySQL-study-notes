b83eb03b880bbcf4f8dbcec5f998cd125a053f10
Richard <richard.yang.tong@gmail.com>
2021-05-27 20:11:01 -0400

    Added notes.

--- Notes_04.sql
+++ Notes_04.sql
@@ -7,4 +7,18 @@
     FROM 表
     WHERE 筛选条件
     ORDER BY 排序列表 [asc|desc]
+    
+特点:
+	1. asc代表升序, desc代表的是降序
+    2. 如果不写, 默认是升序
 */
+
+#案例1: 查询员工信息, 要求工资从高到低排序
+SELECT *
+FROM employees
+ORDER BY salary;
+
+#案例2: 查询员工信息, 要求工资从低到高排序
+SELECT *
+FROM employees
+ORDER BY salary DESC;
