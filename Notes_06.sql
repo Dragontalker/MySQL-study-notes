863e50cba2a4dad8b30f3122c6e1ef8edbe05148
Richard <richard.yang.tong@gmail.com>
2021-05-30 19:33:28 -0400

    Added notes.

--- Notes_06.sql
+++ Notes_06.sql
@@ -45,3 +45,7 @@ FROM employees;
 SELECT SUBSTR('李莫愁爱上了陆展元', 7);
 #截取从指定索引处指定字符长度的字符
 SELECT SUBSTR('李莫愁爱上了陆展元', 1, 3);
+
+#案例: 姓名中首字符大写, 其他字符小写然后用_拼接, 显示出来
+SELECT CONCAT(UPPER(SUBSTR(last_name, 1, 1)), '_', LOWER(SUBSTR(last_name, 2))) AS output
+FROM employees;
