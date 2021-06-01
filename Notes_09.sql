b5e8bbe6b88ee9ee257e5539f817482826e3b22a
Richard <richard.yang.tong@gmail.com>
2021-06-01 06:59:33 -0400

    Added notes.

---
+++ Notes_09.sql
@@ -0,0 +1,12 @@
+#1. 查询公司员工工资的最大值, 最小值, 平均值, 总和
+SELECT 
+	MAX(salary) AS max_salary,
+    MIN(salary) AS min_salary,
+    ROUND(AVG(salary), 2) AS avg_salary,
+    SUM(salary) AS total_salary
+FROM 
+	employees;
+
+#2. 查询员工表中的最大入职时间和最下入职时间的相差天数 (DIFFERENCE)
+
+#3. 查询部门编号为90的员工个数
