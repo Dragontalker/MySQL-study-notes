f5d5a2375d24e39dec6d5f5dbaff69faceda2ac6
Richard <richard.yang.tong@gmail.com>
2021-06-01 06:31:40 -0400

    Added notes.

--- Notes_08.sql
+++ Notes_08.sql
@@ -19,3 +19,7 @@ SELECT
     MIN(salary) AS minimum,
     COUNT(salary) AS count
 FROM employees;
+
+#2. 参数支持哪些类型
+SELECT SUM(last_name), AVG(last_name) FROM employees;
+SELECT SUM(hiredate), AVG(hiredate) FROM employees;
