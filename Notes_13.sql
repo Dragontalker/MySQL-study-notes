ba978b11e2507c2d149d7b9b91a12cd0df0bc418
Richard <richard.yang.tong@gmail.com>
2021-06-03 12:10:22 -0400

    Adde notes.

--- Notes_13.sql
+++ Notes_13.sql
@@ -1,3 +1,8 @@
 #1. 显示员工表的最大工资, 工资平均值
 SELECT MAX(salary), AVG(salary)
 FROM employees;
+
+#2. 查询员工表中的employee_id, job_id, last_name, 按department_id降序, salary升序
+SELECT employee_id, job_id, last_name
+FROM employees
+ORDER BY department_id DESC, salary ASC;
