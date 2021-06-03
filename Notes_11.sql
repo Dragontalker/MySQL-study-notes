cf3ff4e644b640a4b49c03e22bb86eef70025038
Richard <richard.yang.tong@gmail.com>
2021-06-03 08:45:38 -0400

    Adde notes.

--- Notes_11.sql
+++ Notes_11.sql
@@ -8,3 +8,10 @@ ORDER BY job_id;
 #2. 查询员工最高工资和最低工资的差距(DIFFERENCE)
 SELECT MAX(salary) - MIN(salary) AS difference
 FROM employees;
+
+#3. 查询各个管理者手下员工的最低工资, 其中最低工资不能低于6000, 没有管理者的员工计算在内
+SELECT MIN(salary), manager_id
+FROM employees
+WHERE manager_id IS NOT NULL 
+GROUP BY manager_id
+HAVING MIN(salary) >= 6000;
