cd255db5741bf71332dc416c89516b081a7e3874
Richard <richard.yang.tong@gmail.com>
2021-06-03 18:42:44 -0400

    Added notes.

--- Notes_18.sql
+++ Notes_18.sql
@@ -4,3 +4,12 @@
 SELECT department_id
 FROM employees
 WHERE last_name = 'Zlotkey';
+
+#(2)查询部门号=(1)的姓名和工资
+SELECT last_name, salary
+FROM employees
+WHERE department_id = (
+	SELECT department_id
+    FROM employees
+    WHERE last_name = 'Zlotkey'
+);
