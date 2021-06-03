f016e18e967e453293bb4b1916c10df300739d56
Richard <richard.yang.tong@gmail.com>
2021-06-03 16:10:20 -0400

    Added notes.

--- Notes_16.sql
+++ Notes_16.sql
@@ -4,3 +4,10 @@ FROM beauty AS b
 LEFT OUTER JOIN boyds AS bo
 ON b.boyfriend_id = bo.id
 WHERE b.id > 3;
+
+#2. 查询哪个城市没有部门
+SELECT city, d.*
+FROM locations AS l
+LEFT OUTER JOIN departments AS d
+ON l.location_id = d.department_id
+WHERE department_id IS NULL;
