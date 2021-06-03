ae6a54d0a87c9aa333e9613b2ada94633a457c37
Richard <richard.yang.tong@gmail.com>
2021-06-03 14:46:30 -0400

    Adde notes.

--- Notes_15.sql
+++ Notes_15.sql
@@ -4,11 +4,16 @@
 	SELECT 查询列表
     FROM 表1 别名 [连接类型]
     JOIN 表2 别名 ON 连接条件
-    WHERE 筛选条件
-内连接
+    [WHERE 筛选条件]
+    [GROUP BY 分组]
+    [HAVING 筛选条件]
+    [ORDER BY 排序列表]
+    
+分类:
+内连接(*): inner
 外连接
-	左外
-    右外
-    全外
-交叉连接
+	左外(*): left [outer]
+    右外(*): right [outer]
+    全外: full [outer]
+交叉连接: cross
 */
