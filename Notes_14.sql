fbaa338dba46a62155d278024fbd412d182a2301
Richard <richard.yang.tong@gmail.com>
2021-06-03 14:20:23 -0400

    Adde notes.

--- Notes_14.sql
+++ Notes_14.sql
@@ -2,3 +2,9 @@
 SELECT last_name, d.department_id, department_name
 FROM employees AS e, departments AS d
 WHERE e.department_id = d.department_id;
+
+#2. 查询90号部门员工的job_id和90号部门的location_id
+SELECT job_id, location_id, d.department_id
+FROM employees AS e, departments AS d
+WHERE e.department_id = d.department_id
+AND e.department_id = 90;
