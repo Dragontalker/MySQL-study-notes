1f038aebecb6041899fced9f1278ffd832f14675
Richard <richard.yang.tong@gmail.com>
2021-06-04 14:21:06 -0400

    Added notes.

--- Notes_27.sql
+++ Notes_27.sql
@@ -6,8 +6,19 @@
     - 小数: 定点数, 浮点数
     
 字符型:
-	- 较短的文本
-    - 较长的文本
+	- 较短的文本: char, varchar
+    - 较长的文本: textm, blob(较长的二进制数据)
 
+日期型:
+*/
+
+#一、整型
+/*
+分类:
+tinyint (1 byte), 
+smallint (2 bytes), 
+mediumint (3 bytes), 
+int/integer (4 bytes), 
+bigint (5 bytes)
 
 */
