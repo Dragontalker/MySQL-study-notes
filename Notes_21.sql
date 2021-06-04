ae45bbc1780db23e80dda91951f60ef65831bde9
Richard <richard.yang.tong@gmail.com>
2021-06-04 08:38:32 -0400

    Added notes.

--- Notes_21.sql
+++ Notes_21.sql
@@ -3,3 +3,11 @@
 #(1)查询最低的工资
 SELECT MIN(salary)
 FROM employees;
+
+#(2)查询last_name, salary, 要求salary=(1)
+SELECT last_name, salary
+FROM employees
+WHERE salary = (
+	SELECT MIN(salary)
+	FROM employees
+);
