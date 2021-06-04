60594e288bc0bae21dd8de2432ee8daa6472f662
Richard <richard.yang.tong@gmail.com>
2021-06-04 06:55:19 -0400

    Added notes.

--- Notes_19.sql
+++ Notes_19.sql
@@ -14,6 +14,16 @@
     LIMIT offset, size
 		- offset: 要显示条目的起始索引(起始索引从0开始)
         - size: 要显示的条目个数
+        
+特点:
+	(1)limit语句放在查询语句的最后
+    (2)公式: 
+		- 要显示的页数 page
+        - 每页的条目数 size
+        
+        SELECT 查询列表
+        FROM 表
+        LIMIT (page - 1)*size, size
 */
 
 #案例1: 查询前五条员工的信息
