8f1303e1e9b101dff832f5099c1dbef8f2336951
Richard <richard.yang.tong@gmail.com>
2021-06-04 09:35:17 -0400

    Added notes.

--- Notes_22.sql
+++ Notes_22.sql
@@ -7,6 +7,9 @@ union联合 合并： 将多条查询语句的结果合并成一个结果
 UNION
 查询语句2
 ...
+
+应用场景：
+
 */
 
 #引入的案例: 查询部门编号>90或邮箱包含a的员工信息
@@ -19,3 +22,6 @@ OR department_id > 90;
 SELECT * FROM employees WHERE email LIKE '%a%'
 UNION
 SELECT * FROM employees WHERE department_id > 90;
+
+#案例: 查询中国用户中性别为男的信息以及外国用户中性别为男的用户信息
+
