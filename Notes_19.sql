93190bfee4fe1ed3ae48de85dcee41b9c1e44e81
Richard <richard.yang.tong@gmail.com>
2021-06-04 06:40:26 -0400

    Added notes.

---
+++ Notes_19.sql
@@ -0,0 +1,17 @@
+#进阶8: 分页查询
+/*
+应用场景: 当要显示的数据, 一页显示不全, 需要分页提交sql请求
+
+语法:
+	SELECT 查询列表
+    FROM 表
+	[JOIN type] JOIN 表2
+    ON 连接条件
+    WHERE ...
+    GROUP BY ...
+    HAVING ...
+    ORDER BY ...
+    LIMIT offset, size
+		- offset: 要显示条目的起始索引(起始索引从0开始)
+        - size: 要显示的条目个数
+*/
