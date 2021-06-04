7b5ca0c149cd4bdab8ba9efabd68fc587c9a53a7
Richard <richard.yang.tong@gmail.com>
2021-06-04 14:00:20 -0400

    Added notes.

--- Notes_26.sql
+++ Notes_26.sql
@@ -1,6 +1,16 @@
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+USE test;
 #1. 创建表dept1
 DROP TABLE IF EXISTS dept1;
 CREATE TABLE dept1(
 	id INT(7),
     NAME VARCHAR(25)
 );
+
+#2. 将表departments中的数据插入新表dept2中
+CREATE TABLE dept2
+SELECT department_id, department_name
+FROM myemployees.departments;
+
+SELECT * FROM dept2;
