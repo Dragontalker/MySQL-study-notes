ce277a334a1cccf81c9ac77acda43343b3bab66d
Richard <richard.yang.tong@gmail.com>
2021-06-04 08:52:48 -0400

    Added notes.

--- Notes_21.sql
+++ Notes_21.sql
@@ -76,3 +76,14 @@ FROM employees
 GROUP BY department_id
 ORDER BY AVG(salary)
 LIMIT 1;
+
+#(2)查询部门信息
+SELECT d.*, department_id
+FROM departments AS d
+WHERE department_id = (
+	SELECT department_id
+	FROM employees
+	GROUP BY department_id
+	ORDER BY AVG(salary)
+	LIMIT 1
+);
