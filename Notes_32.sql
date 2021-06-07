c63f2f2ed595c132e636939ac8a732287e79c414
Richard <richard.yang.tong@gmail.com>
2021-06-07 09:16:29 -0400

    Added notes.

--- Notes_32.sql
+++ Notes_32.sql
@@ -84,6 +84,7 @@ ON m.department_id = d.department_id;
 
 #二、视图的修改
 /*
+方式一:
 create or replace view 视图名
 as
 查询语句;
@@ -96,3 +97,11 @@ AS
 SELECT AVG(salary), job_id
 FROM employees
 GROUP BY job_id;
+
+/*
+方式二:
+语法:
+alter view 视图名
+as
+查询语句;
+*/
