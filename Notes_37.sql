9ce5e0bba72ae6c9038c454b7025d013110e02f9
Richard <richard.yang.tong@gmail.com>
2021-06-07 18:03:57 -0400

    Added notes.

--- Notes_37.sql
+++ Notes_37.sql
@@ -5,3 +5,11 @@ BEGIN
 	INSERT INTO admin(admin.username, PASSWORD)
     VALUES (userName, loginPwd);
 END $
+
+#二、创建存储过程实现传入女生编号, 返回女神名称和女神电话
+CREATE PROCEDURE test_pro2(IN id INT, OUT NAME VARCHAR(20), OUT phone VARCHAR(20))
+BEGIN
+	SELECT b.NAME, b.phone INTO NAME, phone
+    FROM beauty AS b
+    WHERE b.id = id;
+END $
