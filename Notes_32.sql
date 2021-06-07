b6f05997a2e59f502b99f11b974388e694d5faf1
Richard <richard.yang.tong@gmail.com>
2021-06-07 08:56:25 -0400

    Added notes.

--- Notes_32.sql
+++ Notes_32.sql
@@ -6,3 +6,10 @@ mysql5.1版本出现的新特性, 是通过表动态生成的数据
 
 比如: 舞蹈班和普通班的对比
 */
+
+#案例: 查询姓张的学生名和专业名
+SELECT stuname, major_name
+FROM stuinfo AS s
+INNER JOIN major AS m 
+ON s.major_id = m.id
+WHERE s.stuname LIKE '%张';
