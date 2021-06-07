d23debb16c1dd53c74ed8a7e70f12fb00a2e8ecc
Richard <richard.yang.tong@gmail.com>
2021-06-07 10:52:30 -0400

    Added notes.

--- Notes_34.sql
+++ Notes_34.sql
@@ -4,5 +4,17 @@ bid 整形, 要求主键
 bname 字符型, 要求设置唯一键, 并非空
 price 浮点型, 要求有默认值 10
 btypeId 类型编号, 要求引用bookType表的 id字段
+
+已知bookType表(不用创建), 字段如下:
+id
+name
 */
 
+DROP TABLE IF EXISTS books;
+CREATE TABLE books(
+	bid INT PRIMARY KEY,
+    bname VARCHAR(20) UNIQUE NOT NULL,
+    price FLOAT DEFAULT 10,
+    btypeId INT,
+    FOREIGN KEY(btypeId) REFERENCES bookType(id)
+);
