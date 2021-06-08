28a7be5e5b0cc5939ae33bbc960d92191514b4e0
Richard <richard.yang.tong@gmail.com>
2021-06-08 07:21:58 -0400

    Added notes.

--- Notes_40.sql
+++ Notes_40.sql
@@ -19,7 +19,14 @@ DELIMITER $
 CREATE PROCEDURE test_ranstr_insert(IN insertCount INT)
 BEGIN
 	DECLARE i INT DEFAULT 1; #定义一个循环变量i, 表示插入次数
+    DECLARE str VARCHAR(26) DEFAULT 'abcdefghijklmnopqrtsuvwxyz';
+    DECLARE startIndex INT DEFAULT 1; #代表起始索引
+    DECLARE len INT DEFAULT 1; #代表截取的字符的长度
     WHILE i <= insertCount DO
+        SET startIndex = FLOOR(RAND()*26 + 1);
+        SET len = FLOOR(RAND()*(20 - startIndex + 1));
+        INSERT INTO stringcontent
+        VALUES (SUBSTR(str, startIndex, len));
 		SET i = i + 1;
 	END WHILE;
 END $
